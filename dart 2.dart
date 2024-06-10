1d84921cbab7499c50be3f49e901b4fb1e3702ef
cyberEXPL <159906169+cyberEXPL@users.noreply.github.com>
2024-06-10 14:53:00 +0200

    Update dart 2.dart

--- dart 2.dart
+++ dart 2.dart
@@ -1,7 +1,7 @@
-//if the number is less then 10
+//if the number is less than 10
 void main () {
   var number = 8;
   if (number < 10 ) {
-    print("Your number is less then 10");
+    print("Your number is less than 10");
   }
 }
