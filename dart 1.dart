ab76e220cf49b8a7cc66caf915c3293c0ef86bc6
cyberEXPL <159906169+cyberEXPL@users.noreply.github.com>
2024-06-10 14:41:01 +0200

    Add files via upload

--- dart 1.dart
+++ dart 1.dart
@@ -1,8 +1,8 @@
-//if the number is greater than 10 
+//if the number is less than 10 
 void main() {
-  var number = 80;
+  var number = 8;
   if ( number > 10) {
-    print ("Your number is greater than 10");
+    print ("Your number is less than 10");
   }
-  // print ("Your number is greater than 10");
+  // print ("Your number is less than 10");
 }
